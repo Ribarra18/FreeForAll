f0dc7a14277e78d78993d9cae712fb13abe290a6
Ribarra18 <Renatothefourth@gmail.com>
2023-08-22 13:17:55 -0400

    added more JS

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const myButton = document.getElementById("myButton");
+
+  myButton.addEventListener("click", showAlert);
+  // adding this shit makes the alert appear when el button esta clicked
+  function showAlert() {
+    alert("Hello Wor...Puto!");
+  }
+});
